c6d92895799aaee01c64480a5719b1af50787173
aaksi <yyff91@mail.ru>
2021-02-17 23:01:42 +0300

    added header, banner-section

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,9 @@
 $(function(){
   
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow:'<button class="banner-section__slider-btn banner-section__slider-btn--left" ><img src="images/icons/arrow-left.svg" alt=""></button>',
+        nextArrow:'<button class="banner-section__slider-btn banner-section__slider-btn--right" ><img src="images/icons/arrow-right.svg" alt=""></button>',
+    });
 
 });
