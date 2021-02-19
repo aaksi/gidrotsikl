90c693be45622838e1ec78c7586189aad27e9303
aaksi <yyff91@mail.ru>
2021-02-19 15:34:30 +0300

    added main page

--- app/js/main.js
+++ app/js/main.js
@@ -7,15 +7,27 @@ $(function(){
         nextArrow:'<button class="banner-section__slider-btn banner-section__slider-btn--right" ><img src="images/icons/arrow-right.svg" alt=""></button>',
     });
 
-    $('.search__tabs-item').on('click', function(e){
+    $('.tab').on('click', function(e){
         e.preventDefault();
 
-        $('.search__tabs-item').removeClass('search__tabs-item--active');
-        $('.search__content-item').removeClass('search__content-item--active');
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
 
-        $(this).addClass('search__tabs-item--active');
-        $($(this).attr('href')).addClass('search__content-item--active');
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
 
         
     })
+
+
+    $('.product-item__favorite').on('click', function(){
+        $(this).toggleClass('product-item__favorite--active');
+    });
+
+    $('.products__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow:'<button class="products__slider-btn products__slider-btn--left" ><img src="images/icons/arrow-black-left.svg" alt=""></button>',
+        nextArrow:'<button class="products__slider-btn products__slider-btn--right" ><img src="images/icons/arrow-black-right.svg" alt=""></button>',
+    });
 });
